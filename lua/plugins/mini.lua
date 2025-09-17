78ba19332990d2893b95094a9b6ecf78f461806b
delpantheone <gabrielpaulosilva@gmail.com>
2025-09-17 17:34:36 -0300

    Added mini and treesitter

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,38 @@
+return {
+  { 'nvim-mini/mini.notify',
+		version = false,
+		config = function()
+			require('mini.notify').setup()
+		end,
+	},
+  { 'nvim-mini/mini.surround',
+		version = false,
+		config = function()
+			require('mini.surround').setup()
+		end,
+	},
+  { 'nvim-mini/mini.pairs',
+		version = false,
+		config = function()
+			require('mini.pairs').setup()
+		end,
+	},
+  { 'nvim-mini/mini.move',
+		version = false,
+		config = function()
+			require('mini.move').setup()
+		end,
+	},
+  { 'nvim-mini/mini.ai',
+		version = false,
+		config = function()
+			require('mini.ai').setup()
+		end,
+	},
+  { 'nvim-mini/mini.indentscope',
+		version = false,
+		config = function()
+			require('mini.indentscope').setup()
+		end,
+	},
+}
