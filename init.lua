f312d8bb73bd2096e44049c9a43a6c6038e409b4
delpantheone <gabrielpaulosilva@gmail.com>
2026-02-27 17:07:36 -0300

    Updated init.lua

--- init.lua
+++ init.lua
@@ -6,6 +6,19 @@ if not string.find(vim.o.runtimepath, config_path, 1, true) then
 	vim.opt.runtimepath:prepend(config_path)
 end
 
+-- Clipboard Windows/WSL:
+if vim.fn.has("wsl") == 1 or vim.fn.has("win32") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = { ["+"] = "clip.exe", ["*"] = "clip.exe" },
+		paste = {
+			["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard))",
+			["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard))",
+		},
+		cache_enabled = 0,
+	}
+end
+
 package.path = package.path .. ";" .. lua_path
 
 require("config.lazy")
