54453f88966741d4b0f134608878402f8c65f41d
delpantheone <gabrielpaulosilva@gmail.com>
2026-02-27 17:30:59 -0300

    Updated rest-nvim config

--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -2,11 +2,8 @@ return {
 	"rest-nvim/rest.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.ensure_installed = opts.ensure_installed or {}
-			vim.list_extend(opts.ensure_installed, { "http", "json" })
-		end,
 	},
+	build = "luarocks install xml2lua --local && luarocks install mimetypes --local",
 	opts = {
 		rocks = {
 			hererocks = false,
@@ -14,7 +11,6 @@ return {
 	},
 	config = function(_, opts)
 		require("rest-nvim").setup(opts)
-
 		vim.api.nvim_set_keymap("n", "<leader>rr", ":Rest run<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>rs", ":Rest env select<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>rf", ":Rest env show<CR>", { noremap = true, silent = true })
