30d22293a2435a7d1d09075d853405a5ab5d7850
delpantheone <gabrielpaulosilva@gmail.com>
2026-02-27 17:20:47 -0300

    Updated rest-nvim config

--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -4,10 +4,17 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
 			opts.ensure_installed = opts.ensure_installed or {}
-			table.insert(opts.ensure_installed, "http")
+			vim.list_extend(opts.ensure_installed, { "http", "json" })
 		end,
 	},
-	config = function()
+	opts = {
+		rocks = {
+			hererocks = false,
+		},
+	},
+	config = function(_, opts)
+		require("rest-nvim").setup(opts)
+
 		vim.api.nvim_set_keymap("n", "<leader>rr", ":Rest run<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>rs", ":Rest env select<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>rf", ":Rest env show<CR>", { noremap = true, silent = true })
